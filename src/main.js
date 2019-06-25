1e2036283e7d2d941fb7e0e56c42f536739462b6
xiao yu <csfuchen123@sina.com>
2019-06-25 16:07:37 +0800

    '初始化项目'

--- src/main.js
+++ src/main.js
@@ -2,6 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+import './assets/globel.css'
+
+Vue.use(ElementUI)
+Vue.use(VueAxios, axios)
+
+Vue.axios.defaults.baseURL = 'http://127.0.0.1:11333/api/private/v1'
 
 Vue.config.productionTip = false
 
---
+++ src/views/Login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="login">
+    <el-form class="form">
+      <el-form-item >
+        <el-input ></el-input>
+      </el-form-item>
+      <el-form-item >
+        <el-input ></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button>提交</el-button>
+        <el-button>重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+<script>
+
+</script>
+<style>
+.login {
+  height: 100%;
+  background-color: #294a69;
+}
+.login .form{
+  width:
+}
+</style>
